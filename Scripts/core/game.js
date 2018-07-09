f4073a04187dfbb1b789d94837a7fef24bfb4ee5
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-07-09 13:52:47 -0400

    added Label Class

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -21,16 +21,8 @@
     function Main() {
         console.log("%c Main Function", "font-style:italic; font-size:16px; color:blue;");
         // this is the Label
-        helloLabel = new createjs.Text("Hello, World!", "60px Consolas", "#000000");
-        helloLabel.regX = helloLabel.getBounds().width * 0.5;
-        helloLabel.regY = helloLabel.getBounds().height * 0.5;
-        helloLabel.x = 320;
-        helloLabel.y = 240;
+        helloLabel = new objects.Label("Hello, World!", "60px", "Consolas", "#000000", 320, 240, true);
         stage.addChild(helloLabel);
-        helloLabel.addEventListener("click", function () {
-            console.log("clicked");
-            helloLabel.text = "Clicked!";
-        });
     }
     window.addEventListener("load", Start);
 })();
--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -4,7 +4,7 @@
     // Game Variables
     let canvas:HTMLCanvasElement;
     let stage:createjs.Stage;
-    let helloLabel:createjs.Text;
+    let helloLabel:objects.Label;
 
     function Start():void {
         console.log(`%c Start Function`,"font-weight:bold; font-size:20px; color: red;");
@@ -27,17 +27,8 @@
         console.log(`%c Main Function`,"font-style:italic; font-size:16px; color:blue;");
 
         // this is the Label
-        helloLabel = new createjs.Text("Hello, World!", "60px Consolas", "#000000")
-        helloLabel.regX = helloLabel.getBounds().width * 0.5;
-        helloLabel.regY = helloLabel.getBounds().height * 0.5;
-        helloLabel.x = 320;
-        helloLabel.y = 240;
+        helloLabel = new objects.Label("Hello, World!", "60px", "Consolas", "#000000", 320, 240, true);
         stage.addChild(helloLabel);
-
-        helloLabel.addEventListener("click", function(){
-            console.log(`clicked`);
-            helloLabel.text = "Clicked!";
-        });
     }
 
     window.addEventListener("load", Start);
